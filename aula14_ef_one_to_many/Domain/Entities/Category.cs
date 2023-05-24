dea33f0b840463a67645a8fbccb24d9027f3d3ed
Cássio h Costa <cassio@prouserapps.com>
2023-05-24 08:43:24 -0300

    category

---
+++ aula14_ef_one_to_many/Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aula14_ef_one_to_many.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
